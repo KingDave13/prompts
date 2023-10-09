2d63bb1cc76e1f31d181538725058373e0a965a9
David Okaliwe <okaliwedavid@gmail.com>
2023-10-09 20:31:17 +0100

    Update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+import User from "@models/user";
+import { connectToDb } from "@utils/database";
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -12,7 +15,28 @@ const handler = NextAuth({
 
     },
     async signIn({ profile }) {
+        try {
+            await connectToDb();
 
+            //check if a user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
+            
+            //if not, create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 })
 
