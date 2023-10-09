4a45a59f0bd21d5874e37a1a64a23a6f4cfe5e89
David Okaliwe <okaliwedavid@gmail.com>
2023-10-09 12:51:34 +0100

    Update

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
 
 const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover and Share
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">
+                AI-Powered Prompts
+            </span>
+            <p className="desc text-center">
+                Promptopia is an open source AI prompting tool for
+                the modern world to discover, create and share creative
+                prompts.
+            </p>
+
+            {/* {Feed} */}
+        </h1>
+    </section>
   )
 };
 
