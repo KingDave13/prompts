55faed41e9a4ef1cebe656673f4ee405622dd23e
David Okaliwe <okaliwedavid@gmail.com>
2023-10-09 13:09:28 +0100

    Update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,8 @@ import {
 }  from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-5">
       <Link href='/' className="flex gap-2 flex-center">
@@ -27,7 +29,17 @@ const Nav = () => {
       {/* Mobile Navigation */}
 
       <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href='/create-prompt' className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <>
 
+          </>
+        )}
       </div>
     </nav>
   )
