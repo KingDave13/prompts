6c2269aa5d6b4fb5d622862a237f6a406d7f4999
David Okaliwe <okaliwedavid@gmail.com>
2023-10-10 14:34:51 +0100

    Update

--- components/Form.jsx
+++ components/Form.jsx
@@ -19,7 +19,20 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 
         glassmorphism"
       >
-
+        <label>
+          <span className="font-satoshi font-semibold text-base 
+          text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea 
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, 
+            prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          />
+        </label>
       </form>
     </section>
   )
