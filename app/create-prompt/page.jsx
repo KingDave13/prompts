812d8bcb4c8331c67d94ab13070058af0620353c
David Okaliwe <okaliwedavid@gmail.com>
2023-10-10 14:15:37 +0100

    Update

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const CreatePrompt = () => {
   return (
     <div>page</div>
   )
 }
 
-export default page;
+export default CreatePrompt;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -113,7 +113,7 @@ const Nav = () => {
                   className="dropdown_link"
                   onClick={() => setToggleDropdown(false)}
                 >
-                  Create Prompt
+                  Create Post
                 </Link>
 
                 <button
