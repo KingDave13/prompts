cd78815ca7eb62ffe8a3130ca41eedfee10d0c94
David Okaliwe <okaliwedavid@gmail.com>
2023-10-10 14:44:04 +0100

    Update

--- components/Form.jsx
+++ components/Form.jsx
@@ -60,6 +60,8 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
           <button
             type="submit"
             disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full
+            text-white"
           >
             {submitting ? `${type}...` : type}
           </button>
