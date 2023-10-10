d4dc9831a5356a7e5c1bf6f8b6517020a2448037
David Okaliwe <okaliwedavid@gmail.com>
2023-10-10 14:27:36 +0100

    Update

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <section>
-
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">
+          {type} Post
+        </span>
+      </h1>
+      <p>
+        
+      </p>
     </section>
   )
 };
