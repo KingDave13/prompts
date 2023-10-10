483d1b302cd6c8768c896ce827a1aafd70fc81f2
David Okaliwe <okaliwedavid@gmail.com>
2023-10-10 14:24:14 +0100

    Update

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Link from "next/link";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section>
 
-export default Form
+    </section>
+  )
+};
+
+export default Form;
