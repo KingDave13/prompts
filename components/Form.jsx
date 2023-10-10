4818b7e957295e4963bc8ef03bf3234bd52664ad
David Okaliwe <okaliwedavid@gmail.com>
2023-10-10 14:39:07 +0100

    Update

--- components/Form.jsx
+++ components/Form.jsx
@@ -33,6 +33,24 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className="form_textarea"
           />
         </label>
+
+        <label>
+          <span className="font-satoshi font-semibold text-base 
+          text-gray-700">
+            Tag {` `}
+            <span className="font-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+          <input 
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, 
+            tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
       </form>
     </section>
   )
