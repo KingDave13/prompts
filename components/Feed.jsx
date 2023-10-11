ba03a07ac35e5313d8085728f567c5b59341cf2f
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 12:00:05 +0100

    Update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,6 +3,14 @@
 import { useState, useEffect } from 'react';
 import PromptCard from './PromptCard';
 
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      
+    </div>
+  )
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
 
