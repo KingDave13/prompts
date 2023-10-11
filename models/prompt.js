f949d6899dad52c2c69c9ac3dceefa7e0e3f6761
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 11:30:14 +0100

    Update

---
+++ models/prompt.js
@@ -0,0 +1,16 @@
+import { Schema, model, models } from 'mongoose';
+
+const promptSchema = new Schema({
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    prompt: {
+        type: String,
+        required: [true, 'Prompt is required.'],
+    },
+    tag: {
+        type: String,
+        required: [true, 'Tag is required.'],
+    }
+})
