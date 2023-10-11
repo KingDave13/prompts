79c121971dcc55741b87e5d7c62569edfb29c3d7
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 11:56:13 +0100

    Update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,9 +4,26 @@ import { useState, useEffect } from 'react';
 import PromptCard from './PromptCard';
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+
+  }
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input 
+          type='text'
+          placeholder='Search for a tag or username.'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+    </section>
   )
-}
+};
 
 export default Feed;
