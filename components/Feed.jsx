01403de2c167213d5196e6d4f9fe10f73dfc6060
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 18:19:51 +0100

    Update

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,7 +6,13 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className='mt-16 prompt_layout'>
-
+      {data.map((post) => (
+        <PromptCard 
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
     </div>
   )
 };
@@ -44,7 +50,7 @@ const Feed = () => {
       </form>
 
       <PromptCardList 
-        data={[]}
+        data={[posts]}
         handleTagClick={() => {}}
       />
     </section>
