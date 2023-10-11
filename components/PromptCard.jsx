173b03cca70b16ec6398469dba7293292f508de6
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 21:20:17 +0100

    Update

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -64,6 +64,23 @@ const PromptCard = ({
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 };
