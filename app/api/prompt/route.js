46d5453bef80cd15e8d5a49620fd41963b27cb2b
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 18:22:24 +0100

    Update

---
+++ app/api/prompt/route.js
@@ -0,0 +1,12 @@
+import { connectToDb } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request) => {
+    try {
+        await connectToDb();
+
+        const prompts = 
+    } catch (error) {
+
+    }
+};
