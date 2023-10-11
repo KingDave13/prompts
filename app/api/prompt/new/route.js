2bacc100d40610bc07d82523c89059b1328c1b4f
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 11:25:28 +0100

    Update

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,3 +1,11 @@
-export const POST = async (req, res) => {
+import { connectToDb } from "@utils/database";
 
+export const POST = async (req) => {
+    const { userId, prompt, tag } = await req.json();
+
+    try {
+        await connectToDb();
+    } catch (error) {
+
+    }
 };
