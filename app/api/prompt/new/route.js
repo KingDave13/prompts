168e72bd66e41d3b98fb1a5b24edea24923a2a5c
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 11:38:44 +0100

    Update

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -15,8 +15,10 @@ export const POST = async (req) => {
 
         await newPrompt.save();
 
-        return new Response(JSON.stringify(newPrompt), { status: 201 })
+        return new Response(JSON.stringify(newPrompt), 
+        { status: 201 })
     } catch (error) {
-        
+        return new Response('Failed to create a new prompt', 
+        { status: 500 })
     }
 };
