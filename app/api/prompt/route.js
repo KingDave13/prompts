554aeb630edf58808d6a5bb1bf757e8e203c6a12
David Okaliwe <okaliwedavid@gmail.com>
2023-10-11 18:33:08 +0100

    Update

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,8 +5,12 @@ export const GET = async (request) => {
     try {
         await connectToDb();
 
-        const prompts = 
-    } catch (error) {
+        const prompts = await Prompt.find({}).populate('creator');
 
+        return new Response(JSON.stringify(prompts),
+        {status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts",
+        {status: 500 })
     }
 };
